9eb2ab08cd4647ca254a3b53fd3080cc833e5acd
Ian Maru <iankater@gmail.com>
2021-08-17 19:14:59 +0300

    changed db name

--- src/backend/models/models.go
+++ src/backend/models/models.go
@@ -1,10 +1,17 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type Movie struct {
-	Title     string `json:"title"`
-	Year      int    `json:"year"`
-	Overview  string `json:"overview"`
-	Directors string `json:"directors"`
-	Budget    uint64 `json:"budget"`
-	Gross     uint64 `json:"gross"`
+	gorm.Model
+	Title     string `gorm:"column:title" json:"title"`
+	Year      int    `gorm:"column:year" json:"year"`
+	Overview  string `gorm:"column:overview" json:"overview"`
+	Directors string `gorm:"column:directors" json:"directors"`
+	Budget    uint64 `gorm:"column:budget" json:"budget"`
+	Gross     uint64 `gorm:"column:gross" json:"gross"`
+}
+
+func (Movie) TableName() string {
+	return "test"
 }
